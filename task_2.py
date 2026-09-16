c56172e1107bfe499e9def5b6b873a8fa13ac336
Georgii Efrosinin <2825890@gmail.com>
2026-09-16 13:38:32 +0300

    Правки по комментариям

--- task_2.py
+++ task_2.py
@@ -1,16 +1,17 @@
 class Tester:
 
-    def __init__(self, name, has_deadline=True):
+    def __init__(self, name):
         self.name = name
-        self.deadline = has_deadline
 
-    def work_hard(self):
-        if self.deadline:
+
+    def work_hard(self, has_deadline=True):
+        if has_deadline:
             print(f'{self.name}, Что ж, ещё часок поработаю!')
         else:
             print(f'{self.name}, Можно отдыхать')
 
-tester_1 = Tester(name='tester_1', has_deadline=False)
-tester_1.work_hard()  # 'tester_1 Можно отдыхать'
-tester_2 = Tester(name='tester_2', has_deadline=True)
-tester_2.work_hard()  # 'tester_2 Что ж, ещё часок поработаю!' 
+
+tester_1 = Tester(name='tester_1')
+tester_1.work_hard(has_deadline=False)  # 'tester_1, Можно отдыхать'
+tester_2 = Tester(name='tester_2')
+tester_2.work_hard(has_deadline=True)   # 'tester_2, Что ж, ещё часок поработаю!'
